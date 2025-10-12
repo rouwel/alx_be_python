15640e1d4d7a136438cc416d50e6e576c580e07c
Rouwel <rouwelngacha@gmail.com>
2025-10-12 16:59:49 +0300

    Add to version control

---
+++ arithmetic_operations.py
@@ -0,0 +1,23 @@
+def perform_operation(num1:float, num2:float, operation:str) -> float:
+    if operation == "add":
+        return num1+num2
+    elif operation == "subtract":
+        return num1-num2
+    elif operation == "multiply":
+        return num1*num2
+    elif operation == "divide":
+        if num2 == 0:
+            raise ValueError("Cannot divide by zero")
+        return num1/num2
+    else:
+        return "Error: Invalid operation"
+print(perform_operation(13,5,"multiply"))
+
+
+
+
+
+
+
+
+
