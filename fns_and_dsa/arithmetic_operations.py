f7ad5c55efd01dc7c2d00a6eac2b15420700055f
Rouwel <rouwelngacha@gmail.com>
2025-10-15 08:12:50 +0300

    Final changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,16 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     if operation == "add":
-        return num1+num2
+        return num1 + num2
     elif operation == "subtract":
         return num1-num2
     elif operation == "multiply":
         return num1*num2
     elif operation == "divide":
         if num2 == 0:
-            raise ValueError("Cannot divide by zero")
+            return ("Error: Cannot divide by zero")
         return num1/num2
     else:
         return "Error: Invalid operation"
-print(perform_operation(13,5,"multiply"))
 
 
 
